51d405b9a7f73d6a80117f33ddbabf2f37754c9d
davidjay106 <60650437+davidjay106@users.noreply.github.com>
2022-12-02 12:19:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App Vp-7!')
+  res.send('CICD App Vp-8!')
 })
 
 app.get('/status', (req, res) => {
